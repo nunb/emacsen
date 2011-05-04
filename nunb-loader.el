9964417191285a58dff222881225978971761256
Nandan Bagchee <nandan@infinitelybeta.com>
2011-05-04 17:48:16 +0530

    - remove cruft

--- nunb-loader.el
+++ nunb-loader.el
@@ -3,20 +3,12 @@
 (setq loader-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 
-;; Demonstrate failed experiments with current directory
-;; 
-;; (defun current-dir ()
-;;   (file-name-directory
-;;    (or (buffer-file-name) load-file-name)))
-;; (setq loader-dir (current-dir))
-
 ;; FAILED EXPERIMENTS
 ;; (setq loader-dir default-directory) 
 ;; --> no, this is the failure, because it doesn't work when -q -l opts are used.
 
-;; Add all subdirs in a given dir to the load-path, defaulting to this file's dir.
-;; (or emacsen (horror! Huge subtree below ~/emacsen!))
-;; 
+
+;; defaulting to this file's dir, or emacsen 
 (defun nunb-set-loadpath-all-under (loaddir)
   "Takes a dir, and loads all of its subdirs into the load-path"
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
@@ -28,9 +20,6 @@
 	(nconc load-path orig-load-path))))
 
 
-;; example (nunb-set-loadpath-all-under "/tmp/")
-
-load-path
 ;; Load user and system specific customizations
 (setq system-specific-config (concat loader-dir system-name ".el")
       user-specific-config (concat loader-dir user-login-name ".el")
