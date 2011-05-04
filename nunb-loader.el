f0ebc46f46814b3406017776bbb450acf9121685
Nandan Bagchee <nandan@infinitelybeta.com>
2011-05-04 16:56:48 +0530

    - use Vedang's version (but results need testing)

--- nunb-loader.el
+++ nunb-loader.el
@@ -4,13 +4,16 @@
                     (or (buffer-file-name) load-file-name)))
 
 
-;; Add all subdirs in a given dir to the load-path, defaulting to this file's dir
+;; Add all subdirs in a given dir to the load-path, defaulting to this file's dir, or emacsen (horror! Huge subtree below ~/emacsen!)
 (defun nunb-set-loadpath-all-under (loaddir)
- "Takes a dir, and loads all of its subdirs into the load-path"
+  "Takes a dir, and loads all of its subdirs into the load-path"
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (let* ((loadit (or loaddir loader-dir)))
-	(setq load-path (cons loadit load-path))
-	(normal-top-level-add-subdirs-to-load-path))))
+      (let* ((my-lisp-dir (or loaddir loader-dir "~/emacsen/"))
+	     (default-directory my-lisp-dir)
+	     (orig-load-path load-path))
+	(setq load-path (cons my-lisp-dir nil))
+	(normal-top-level-add-subdirs-to-load-path)
+	(nconc load-path orig-load-path))))
 
 (nunb-set-loadpath-all-under "/tmp/")
 
