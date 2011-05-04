fb586410c4c45f7dbe129ee07e6a6f8ee210062e
Nandan Bagchee <nandan@infinitelybeta.com>
2011-05-04 16:51:48 +0530

    - tmp testing changes, but it won't work as expected because default-directory is a dynvar which MUST be set for normal-top-* fn to work. Vedang's version is better.

--- nunb-loader.el
+++ nunb-loader.el
@@ -1,18 +1,22 @@
-;; Add all subdirs in a given dir to the load-path
-
-(defun nunb-set-loadpath-all-under (loaddir)
- "Takes a dir, and loads all of its subdirs into the load-path"
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (let* ((default-directory (or loaddir "~/emacsen/"))
-	     (setq load-path (cons my-lisp-dir load-path))
-	     (normal-top-level-add-subdirs-to-load-path))))
-
 
 ;; Where is this nunb-loader file?
 (setq loader-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 
 
+;; Add all subdirs in a given dir to the load-path, defaulting to this file's dir
+(defun nunb-set-loadpath-all-under (loaddir)
+ "Takes a dir, and loads all of its subdirs into the load-path"
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (let* ((loadit (or loaddir loader-dir)))
+	(setq load-path (cons loadit load-path))
+	(normal-top-level-add-subdirs-to-load-path))))
+
+(nunb-set-loadpath-all-under "/tmp/")
+
+(require 'parenface)
+
+load-path
 ;; Load user and system specific customizations
 (setq system-specific-config (concat loader-dir system-name ".el")
       user-specific-config (concat loader-dir user-login-name ".el")
@@ -22,7 +26,9 @@
 
 (defun nunb-load-system-and-user () ;; From ESK
   (if (file-exists-p system-specific-config) (load system-specific-config))
-  (if (file-exists-p user-specific-config) (load user-specific-config))
+  (if (file-exists-p user-specific-config)
+      (message "fuck you")
+      (load user-specific-config))
   (if (file-exists-p user-specific-dir)
       (mapc #'load (directory-files user-specific-dir nil ".*el$"))))
 
