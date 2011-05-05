2f7ee1964ce2770ee13bbc81e512f2e1e61509fd
Nandan Bagchee <nandan@infinitelybeta.com>
2011-05-05 11:47:55 +0530

    - To add old Aquamacs setup, just make a new vendor and copy all files into that. Also, my username is Nandan, apparently, so this file should be Nandan.el, but whatevs

--- nandan.el
+++ nandan.el
@@ -3,6 +3,17 @@
 ;; (when (boundp 'aquamacs-version)
 ;;   (one-buffer-one-frame-mode))
 
-(require 'paredit)
+;; If I want the original files location, I use this, otherwise
+;; I've put the old files into vendors/oldAquamacs
+;; (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+;;      (let* ((my-lisp-dir "~/nb/elisp-and-emacs/")
+;;            (default-directory my-lisp-dir))
+;;         (setq load-path (cons my-lisp-dir load-path))
+;;         (normal-top-level-add-subdirs-to-load-path)))
 
+(require 'nunb)
+(require 'lisper)
+(lp)
 (global-set-key [C-o] 'other-window)
+
+(switch-to-buffer "*Messages*")
