061d2db917664a2c8036c245573724021c42b47c
Nandan Bagchee <nandan@infinitelybeta.com>
2011-05-06 12:44:42 +0530

    - wtf

--- tallinn.el
+++ tallinn.el
@@ -5,12 +5,12 @@
 (autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 
-(add-to-list 'auto-mode-alist '("\\.clj$" . paredit-mode))
+;; (add-to-list 'auto-mode-alist '("\\.clj$" . paredit-mode))
 
 
-(setq auto-mode-alist (cons '("\\.clj$" . paredit-mode) auto-mode-alist))
+;; (setq auto-mode-alist (cons '("\\.clj$" . paredit-mode) auto-mode-alist))
 
-(setq auto-mode-alist '(("\\.jade$" . jade-mode) ("\\.haml$" . haml-mode) ("\\.gemspec$" . ruby-mode) ("Rakefile$" . ruby-mode) ("\\.rake$" . ruby-mode) ("\\.rxml$" . ruby-mode) ("\\.rjs$" . ruby-mode) ("\\.applescript$" . applescript-mode) ("\\.yml$" . yaml-mode) ("\\.rb$" . ruby-mode) ("\\.clj$" . clojure-mode) ("\\.cp\\'" . c++-mode)))
+;; (setq auto-mode-alist '(("\\.jade$" . jade-mode) ("\\.haml$" . haml-mode) ("\\.gemspec$" . ruby-mode) ("Rakefile$" . ruby-mode) ("\\.rake$" . ruby-mode) ("\\.rxml$" . ruby-mode) ("\\.rjs$" . ruby-mode) ("\\.applescript$" . applescript-mode) ("\\.yml$" . yaml-mode) ("\\.rb$" . ruby-mode) ("\\.clj$" . clojure-mode) ("\\.cp\\'" . c++-mode)))
 
 ;;;   (add-to-list 'load-path "/path/to/elisp")
 ;;;   (autoload 'enable-paredit-mode "paredit"
@@ -20,4 +20,10 @@
 ;;; Enable Paredit Mode on the fly with `M-x enable-paredit-mode RET',
 ;;; or always enable it in a major mode `M' (e.g., `lisp') with:
 ;;;
- (add-hook clojure-mode-hook 'enable-paredit-mode)
+
+;; (add-hook clojure-mode-hook 'enable-paredit-mode)
+
+;; (setq clojure-mode-hook '())
+
+;; clojure-mode-hook
+
